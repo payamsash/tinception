f585902b3a74304d1c6f9b2ac1f384efec2c8862
payam <payam.sadeghishabestari@uzh.ch>
2025-04-17 11:39:55 +0200

    tinmeg added

--- conversion.sh
+++ conversion.sh
@@ -6,7 +6,7 @@
 
 # setup FS
 export FREESURFER_HOME=/usr/local/freesurfer/8.0.0
-export SUBJECTS_DIR=/home/ubuntu/data/tinception/subjects_fs_dir
+export SUBJECTS_DIR=/home/ubuntu/volume/subjects_fs_dir
 export LD_LIBRARY_PATH=$FREESURFER_HOME/MCRv97/runtime/glnxa64:$FREESURFER_HOME/MCRv97/bin/glnxa64:$FREESURFER_HOME/MCRv97/sys/os/glnxa64:$FREESURFER_HOME/MCRv97/extern/bin/glnxa64
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export FS_V8_XOPTS=0
@@ -33,7 +33,7 @@ subjects_raw="/home/ubuntu/data/tinception/subjects_raw"
 cd $subjects_raw
 nohup bash -c 'ls *.mgz | parallel --jobs 20 recon-all -s {.} -i {} -all' &
 
-###### TINSPECT
+###### TINSPECT & TINMEG
 ## .hdr to nifti
 tinspect_dir="/home/ubuntu/data/tinception/TINSPECT"
 tinspect_subjects_dir="/home/ubuntu/data/tinception/subjects_raw"
@@ -51,9 +51,7 @@ done
 
 subjects_raw="/home/ubuntu/data/tinception/subjects_raw"
 cd $subjects_raw
-nohup bash -c 'ls *.mgz | parallel --jobs 21 recon-all -s {.} -i {} -all -notal-check' &
-
-###### TINMEG
+nohup bash -c 'ls *.mgz | parallel --jobs 10 "subject={.}; [[ \$subject == MR* ]] && flag=\"-notal-check\" || flag=\"\"; recon-all -s \$subject -i {} -all \$flag"' &
 
 ###### COGFAIL
 
