9b103d5d19f4bc4029a3b8050480e235ee379e22
payam <payam.sadeghishabestari@uzh.ch>
2025-07-10 15:32:38 +0200

    stats updated

--- tools/stats.R
+++ tools/stats.R
@@ -4,38 +4,58 @@ library(openxlsx)
 library(readr)
 library(gtsummary)
 library(ggplot2)
+library(dplyr)
 
-data <- read_csv("./material/with_qc/behavioural/tinception_master_ready_for_matching_qc.csv")
-data$group <- as.factor(data$group)
-data$sex <- as.factor(data$sex)
-data$site <- as.factor(data$site)
+data <- read_excel("/Users/payamsadeghishabestari/Downloads/PTA_values.xlsx")
+data <- na.omit(data)
+data$Group <- as.factor(data$Group)
+data$Sex <- as.factor(data$Sex)
+
+data <- data %>%
+  mutate(PTA4_avg = rowMeans(select(., RPTA4, LPTA4), na.rm = TRUE)) %>%
+  select(-RPTA4, -LPTA4)
+
+data <- data %>%
+  mutate(HF_avg = rowMeans(select(., RHF, LHF), na.rm = TRUE)) %>%
+  select(-RHF, -LHF)
+
+
+# data$site <- as.factor(data$site)
 
 ## matching
 match_out <- matchit(
-                    group ~ age + sex + site + PTA, 
+                    Group ~ Age + Sex + PTA4_avg + HF_avg, 
                     data = data, 
-                    method = "nearest",
-                    distance = "glm"
+                    method = "optimal",
+                    distance = "glm",
+                    ratio=2
                     )
 
 matched_data <- match.data(match_out) %>% 
     dplyr::filter(weights > 0)
 
-#write.xlsx(matched_data, "./material/with_qc/behavioural/tinception_matched_optimal.xlsx")
-#write_lines(unique(matched_data$`subject ID`), "./material/with_qc/behavioural/tinception_matched_optimal.txt")
+matched_data <- matched_data %>%
+  group_by(subclass) %>%
+  filter(all(distance < 0.8)) %>%
+  ungroup()
+
+# write.xlsx(matched_data, "./material/with_qc/behavioural/tinception_matched_optimal.xlsx")
+# write_lines(unique(matched_data$`subject ID`), "./material/with_qc/behavioural/tinception_matched_optimal.txt")
+
+write.xlsx(matched_data, "/Users/payamsadeghishabestari/Downloads/matched_optimal_glm.xlsx")
 
 ## plotting
 quartz()
 plot(summary(match_out))
 plot(match_out, type = "jitter", interactive = FALSE)
 plot(match_out, type = "density", interactive = FALSE,
-                    which.xs = ~age + site + sex)
+                    which.xs = ~Age + Sex + HF_avg)
 
 ## stat summery
-summary_tbl <- matched_data %>%
-    select(group, age, PTA, sex, site) %>%
+summary_tbl <- data %>%
+    select(Group, Age, PTA4_avg, HF_avg, Sex) %>%
     tbl_summary(
-        by = group,  
+        by = Group,  
         statistic = list(
             all_continuous() ~ "{mean} ± {sd}",  
             all_categorical() ~ "{n} ({p}%)"     
@@ -45,4 +65,4 @@ summary_tbl <- matched_data %>%
     add_p() %>%  
     bold_labels()  
 
-summary_tbl
+# summary_tbl
