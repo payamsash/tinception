c8817ad587c1de7a51df55f01bfde59f1df25cba
payam <payam.sadeghi74@gmail.com>
2026-03-23 13:16:53 +0100

    merging for 11 brain region add

--- src/ukb/src/02_gwas.sh
+++ src/ukb/src/02_gwas.sh
@@ -222,3 +222,120 @@ for pheno in PC1_brain PC2_brain; do
 
 done
 
+############# only for brain volumes
+exome_file_dir="/Bulk/Exome sequences/Population level exome OQFE variants, PLINK format - interim 450k release/"
+data_file_dir="/GWAS/results_brain"
+pheno_dir="/GWAS/main"
+field_name="ukb23149"
+
+for chr in {1..22}; do
+    run_plink_wes="plink2 --bfile ${field_name}_c${chr}_b0_v1 \
+        --no-pheno --keep pheno_regions_raw.tsv --autosome \
+        --maf 0.01 --mac 20 --geno 0.1 --hwe 1e-15 --mind 0.1 \
+        --write-snplist --write-samples --no-id-header \
+        --out WES_c${chr}_snps_qc_pass"
+
+    dx run swiss-army-knife \
+        -iin="${exome_file_dir}/${field_name}_c${chr}_b0_v1.bed" \
+        -iin="${exome_file_dir}/${field_name}_c${chr}_b0_v1.bim" \
+        -iin="${exome_file_dir}/${field_name}_c${chr}_b0_v1.fam" \
+        -iin="${pheno_dir}/pheno_regions_raw.tsv" \
+        -icmd="${run_plink_wes}" \
+        --tag="Step2_QC" \
+        --name "QC_WES_chr${chr}" \
+        --instance-type "mem1_ssd1_v2_x16" \
+        --destination="${data_file_dir}/" \
+        --brief --yes
+done
+
+############# only for brain volumes
+exome_file_dir="/Bulk/Exome sequences/Population level exome OQFE variants, PLINK format - interim 450k release/"
+data_file_dir="/GWAS/results_brain"
+pheno_dir="/GWAS/main"
+field_name="ukb23149"
+
+
+for pheno in Accessory-Basal-nucleus-lh Basal-nucleus-lh Lateral-nucleus-lh GC-ML-DG-body-rh presubiculum-head-rh MDm-lh MDm-rh MDl-lh CeM-rh CeM-lh PuA-lh; do
+    for chr in {1..22}; do
+        run_regenie_cmd="regenie --step 2 \
+            --bed ${field_name}_c${chr}_b0_v1 \
+            --out ${pheno}_assoc.c${chr} \
+            --phenoFile pheno_regions_raw.tsv \
+            --covarFile covariates_regions_with_srt.tsv \
+            --extract WES_c${chr}_snps_qc_pass.snplist \
+            --phenoCol ${pheno} \
+            --covarCol age --covarCol genotype --covarCol srt --covarCol eTIV \
+            --covarCol PC_1 --covarCol PC_2 --covarCol PC_3 --covarCol PC_4 --covarCol PC_5 \
+            --covarCol PC_6 --covarCol PC_7 --covarCol PC_8 --covarCol PC_9 --covarCol PC_10 \
+            --pred tinnitus_results_pred.list \
+            --bsize 200 \
+            --minMAC 3 \
+            --threads 16 \
+            --gz"
+
+        dx run swiss-army-knife \
+            -iin="${exome_file_dir}/${field_name}_c${chr}_b0_v1.bed" \
+            -iin="${exome_file_dir}/${field_name}_c${chr}_b0_v1.bim" \
+            -iin="${exome_file_dir}/${field_name}_c${chr}_b0_v1.fam" \
+            -iin="${data_file_dir}/WES_c${chr}_snps_qc_pass.snplist" \
+            -iin="${pheno_dir}/pheno_regions_raw.tsv" \
+            -iin="${pheno_dir}/covariates_regions_with_srt.tsv" \
+            -iin="${data_file_dir}/tinnitus_results_pred.list" \
+            -iin="${data_file_dir}/tinnitus_results_1.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_2.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_3.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_4.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_5.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_6.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_7.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_8.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_9.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_10.loco.gz" \
+            -iin="${data_file_dir}/tinnitus_results_11.loco.gz" \
+            -icmd="${run_regenie_cmd}" \
+            --tag="Step2_Assoc" \
+            --instance-type "mem1_ssd1_v2_x16" \
+            --name "regenie_${pheno}_chr${chr}" \
+            --destination="${data_file_dir}/" \
+            --brief --yes
+    done
+done
+
+############# merge for 11 brain regions
+for pheno in \
+Accessory-Basal-nucleus-lh \
+Basal-nucleus-lh \
+Lateral-nucleus-lh \
+GC-ML-DG-body-rh \
+presubiculum-head-rh \
+MDm-lh \
+MDm-rh \
+MDl-lh \
+CeM-rh \
+CeM-lh \
+PuA-lh; do
+
+    merge_cmd="out_file=\"${pheno}_assoc_merged.txt\"
+    cp /mnt/project/GWAS/results_brain/${pheno}_assoc.c*.regenie.gz .
+    gunzip *.regenie.gz
+    echo -e \"CHROM\tGENPOS\tID\tALLELE0\tALLELE1\tA1FREQ\tN\tTEST\tBETA\tSE\tCHISQ\tLOG10P\tEXTRA\" > \$out_file
+    files=\"./*.regenie\"
+    for f in \$files
+    do
+        tail -n+2 \$f | tr \" \" \"\t\" >> \$out_file
+    done
+
+    rm *.regenie"
+
+    data_file_dir="/GWAS/results_brain"
+
+    dx run swiss-army-knife \
+        -iin="${data_file_dir}/${pheno}_assoc.c1_${pheno}.regenie.gz" \
+        -icmd="${merge_cmd}" \
+        --tag="Final_Merge" \
+        --name "merge_${pheno}" \
+        --instance-type "mem1_ssd1_v2_x16" \
+        --destination="${data_file_dir}" \
+        --brief --yes
+
+done
